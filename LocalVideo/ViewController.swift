e16fcc7b944c8c1bbe25d6bf0b0bd65324c1aec8
your name <your@email.com>
2016-01-29 15:31:45 +0100

    Editing on Record Function

--- LocalVideo/ViewController.swift
+++ LocalVideo/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
         
         self.presentViewController(playerViewController, animated: true) {
             
+            setupRecorder()
             playerViewController.player?.play()
         
         }
